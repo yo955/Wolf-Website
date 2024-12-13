35b1e6b312c8302ac06e5c8d5f6efa7815d6b5b4
yo955 <yo955@gmail.com>
2024-12-13 23:12:55 +0200

    edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
-import "@/styles"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/_main.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="">{children}</body>
     </html>
   );
 }
