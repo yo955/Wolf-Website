22b3ce88cfcde9a6ca980b6609a7209d5ec2241e
yo955 <yo955@gmail.com>
2024-12-17 14:54:00 +0200

    finish

--- src/components/Atoms/SwipperAtoms/SwipperContainer.tsx
+++ src/components/Atoms/SwipperAtoms/SwipperContainer.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 import { Swiper } from "swiper/react";
-// import Swiper and modules styles
+// استيراد الأنماط الخاصة بالـ Swiper
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules"; 
+
 interface SwipperContainerprops {
   children: React.ReactNode;
   loop?: true;
-  autoplay?: true;
+  autoplay?: boolean;
   spaceBetween?: number;
   slidesPerView: number;
 }
+
 export const SwipperContainer: React.FC<SwipperContainerprops> = ({
   children,
   loop,
@@ -21,11 +24,12 @@ export const SwipperContainer: React.FC<SwipperContainerprops> = ({
   return (
     <Swiper
       loop={loop}
-      autoplay={autoplay}
+      autoplay={autoplay ? { delay: 2500, disableOnInteraction: false } : false}
       slidesPerView={slidesPerView}
       spaceBetween={spaceBetween}
       pagination={{ clickable: true }}
-      
+      modules={[Autoplay]} 
+      // navigation
     >
       {children}
     </Swiper>
--- src/components/Molecules/slider/ImageSlider.tsx
+++ src/components/Molecules/slider/ImageSlider.tsx
@@ -7,20 +7,26 @@ import {
 export const ImageSlider: React.FC = () => {
   const mainbath = "/images/slider";
   const slidesImgs = [];
-  for (let i = 1; i <= 3; i++) {
-    slidesImgs.push(`${mainbath}/swipper-${i}.png`);
+  for (let i = 1; i <= 5; i++) {
+    const imagePath = `${mainbath}/swipper-${i}.png`;
+    if (imagePath) slidesImgs.push(imagePath);
   }
+
   return (
-    <SwipperContainer slidesPerView={5} spaceBetween={50}>
+    <SwipperContainer slidesPerView={5} spaceBetween={50} autoplay={true}>
       <SwipperSlide>
         <div className="slide-content flex gap-10">
-          {slidesImgs.map((img, index) => (
-            <SlideContent
-              key={index}
-              imageUrl={img}
-              alt="sliderImg"
-            ></SlideContent>
-          ))}
+          {slidesImgs.length > 0 ? (
+            slidesImgs.map((img, index) => (
+              <SlideContent
+                key={index}
+                imageUrl={img}
+                alt="sliderImg"
+              ></SlideContent>
+            ))
+          ) : (
+            <div>No images available</div>
+          )}
         </div>
       </SwipperSlide>
     </SwipperContainer>
